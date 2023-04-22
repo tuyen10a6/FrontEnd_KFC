e9bda1f21edc16b5e5f9c6bb9e8be5ecee884fce
tuyen10a6 <phamtuyenok2002@gmail.com>
2023-04-22 22:55:07 +0700

    Get API SlideHome Susessfully

--- JS/home.js
+++ JS/home.js
@@ -1,2 +1,14 @@
 var app = angular.module("myApp", []);
-app.controller("myCtrl", function ($scope) {});
+app.controller("myCtrl", function ($scope, $http) {
+  $http.get("https://localhost:7229/api/Slide/GetAllSlide").then(
+    function (response) {
+      // Xử lý dữ liệu trả về tại đây nếu cần
+      $scope.slides = response.data;
+      console.log(response.data);
+    },
+    function (error) {
+      // Xử lý lỗi tại đây nếu cần
+      console.log(error);
+    }
+  );
+});
