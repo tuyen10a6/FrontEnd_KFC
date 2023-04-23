71b5481fdd8fa967b2e27e9f96db2516220b964e
tuyen10a6 <phamtuyenok2002@gmail.com>
2023-04-23 21:37:09 +0700

    Create Order Successfully

--- JS/Cart.js
+++ JS/Cart.js
@@ -12,4 +12,52 @@ app.controller("Cart", function ($scope, $http, $window) {
       console.log(error);
     }
   );
+  $scope.createOrder = function () {
+    // Tạo đối tượng khách hàng từ dữ liệu trong form
+    var customer = {
+      customerid: 0,
+      customername: $scope.customername,
+      customerphone: $scope.customerphone,
+      customeremail: $scope.customeremail,
+      customeraddress: $scope.customeraddress,
+      maChiNhanh: $scope.customerbranch,
+      orderstatusid: 1,
+    };
+
+    // Lấy thông tin giỏ hàng từ local storage
+    var cartItems = JSON.parse(localStorage.getItem("cartItems"));
+
+    // Tạo một mảng chứa các sản phẩm trong giỏ hàng
+    var orderDetails = [];
+
+    for (var i = 0; i < cartItems.length; i++) {
+      var orderDetail = {
+        maSanPham: cartItems[i].maSanPham,
+        quantity: cartItems[i].soLuong,
+        price: cartItems[i].giaTien,
+      };
+
+      orderDetails.push(orderDetail);
+    }
+
+    // Gửi dữ liệu đơn hàng đến API
+    var data = {
+      khach: customer,
+      listchitiet: orderDetails,
+    };
+
+    $http
+      .post("https://localhost:7229/api/Order/CreateOrder", data)
+      .then(function (response) {
+        // Xử lý kết quả trả về nếu cần
+        console.log(response);
+        localStorage.removeItem("cartItems");
+        alert("Thêm đơn hàng thành công");
+      })
+      .catch(function (error) {
+        // Xử lý lỗi nếu có
+        console.log(error);
+        alert("Lỗi");
+      });
+  };
 });
